10fddc1b32bdde2bc235fee943f381c03361925b
Paul Asquin <paul.asquin@gmail.com>
2018-12-09 13:48:31 +0100

    removing old structure

--- tests/test_env.py
+++
@@ -1,7 +0,0 @@
-from utilities.EnvReader import get_api_key
-from utilities.Errors import EnvError
-
-if not isinstance(get_api_key(), str):
-    raise EnvError()
-else:
-    print("Success!")
--- tests/test_preprocessing.py
+++
@@ -1,8 +0,0 @@
-from classification.preprocessing import Preprocessing
-
-image_path = 'dataset/training/anopheles/pic_014.jpg'
-
-preprocessing = Preprocessing(image_path)
-
-framed = preprocessing.save_framed_img("tests/pic_014_framed.jpg")
-crop_resized = preprocessing.save_crop_img("tests/pic_014_cropped.jpg")
