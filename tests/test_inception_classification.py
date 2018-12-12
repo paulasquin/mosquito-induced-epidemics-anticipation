a393860ad063a5a21e864e6733c689fba7baa25b
Paul Asquin <paul.asquin@gmail.com>
2018-12-12 15:22:33 +0100

    update name in test inception classification

--- tests/test_inception_classification.py
+++ tests/test_inception_classification.py
@@ -5,11 +5,11 @@ import sys
 PATH_TEST_IMG = "pic_014_cropped.jpg"
 
 
-def test_command_retrain():
+def test_inception_retrain():
     command_classification.train_and_monitor()
 
 
-def test_label_image(path_img):
+def test_inception_label_image(path_img):
     print(command_classification.label_automatic(path_img))
 
 
@@ -17,14 +17,14 @@ if __name__ == "__main__":
 
     if len(sys.argv) > 1 and sys.argv[1] == "--retrain":
         print("Testing command_retrain")
-        test_command_retrain()
+        test_inception_retrain()
     elif len(sys.argv) > 1 and sys.argv[1] == "--label":
         print("Testing image labelling")
         if len(sys.argv) > 2:
             for path_img in sys.argv[2:]:
-                test_label_image(path_img)
+                test_inception_label_image(path_img)
         else:
-            test_label_image(PATH_TEST_IMG)
+            test_inception_label_image(PATH_TEST_IMG)
     else:
         if len(sys.argv) > 1 and (sys.argv[1] == "--help" or sys.argv[1] == "-h"):
             print("Didn't recognize command : " + sys.argv[1])
