30bf23b57496bf99bbbc3fcc40b52b86c995f214
Paul Asquin <paul.asquin@gmail.com>
2018-12-14 19:34:20 +0100

    new dataset structure with validation, train and test. New architecture supported by augment and preprocessing, still keeping retrocompatibility

--- image_recognition/image_augmenting.py
+++ image_recognition/image_augmenting.py
@@ -10,82 +10,136 @@ import glob
 
 FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
 LES_AUGMENTATION = ['width-flip', 'height-flip', 'cwRotate', 'ccwRotate', 'inverse']
-DATASET_FOLDER = FOLDER_PATH + "/preprocessed_dataset"
-AUGMENTED_FOLDER = DATASET_FOLDER + "_augmented"
-create_folder(AUGMENTED_FOLDER)
+DATASET_PATH = FOLDER_PATH + "/preprocessed_dataset"
+AUGMENTED_FOLDER = DATASET_PATH + "_augmented"
 
 
-def get_augmentation_path(img_path, augmentation):
-    """ Generate the augmented image path, with given original path and augmentation """
-    augmentation_path = \
-        AUGMENTED_FOLDER \
-        + "/" + img_path.split("/")[-2] + "/" \
-        + img_path.split("/")[-1].replace(".jpg", "-" + augmentation + ".jpg")
-    return augmentation_path
+class Augmentation:
+    @staticmethod
+    def get_augmentation_path(img_path, augmentation, sub_augmented_path):
+        """ Generate the augmented image path, with given original path and augmentation """
+        label = img_path.split("/")[-2]
 
+        # computing augmentation path name
+        if ".jpg" in img_path:
+            augmentation_name = img_path.split("/")[-1].replace(".jpg", "-" + augmentation + ".jpg")
+        elif ".jpeg" in img_path:
+            augmentation_name = img_path.split("/")[-1].replace(".jpeg", "-" + augmentation + ".jpeg")
+        else:
+            print("Error with " + img_path)
+            raise TypeError("image should only be jpg or jpeg")
 
-def not_already_augmented(img_path, augmentation):
-    """ Return False if asked augmentation already exists or if the file is already an augmentation"""
-    aug_path = get_augmentation_path(img_path=img_path, augmentation=augmentation)
-    augmented = False
-    for aug in LES_AUGMENTATION:
-        if "-" + aug in img_path:
-            augmented = True
-    return not (os.path.isfile(aug_path) or augmented)
+        augmentation_path = sub_augmented_path + "/" + label + "/" + augmentation_name
+        return augmentation_path
 
+    @staticmethod
+    def not_already_augmented(img_path, augmentation, sub_augmented_path):
+        """ Return False if asked augmentation already exists or if the file is already an augmentation"""
+        aug_path = Augmentation.get_augmentation_path(
+            img_path=img_path,
+            augmentation=augmentation,
+            sub_augmented_path=sub_augmented_path
+        )
+        augmented = False
+        for aug in LES_AUGMENTATION:
+            if "-" + aug in img_path:
+                augmented = True
+        return not (os.path.isfile(aug_path) or augmented)
 
-def augment_image(les_img_path):
-    """ Apply augmentation operations defined by LES_AUGMENTATION corresponding to PIL transformations"""
-    global LES_AUGMENTATION
-    print(', '.join(LES_AUGMENTATION))
-    for i, img_path in enumerate(les_img_path):
-        with PIL.Image.open(img_path) as img:
-            print(str(i + 1) + "/" + str(len(les_img_path)) + " : Augmenting " + img_path.split("/")[-1] + " "*30, end="\r")
-            for augmentation in LES_AUGMENTATION:
-                if augmentation == 'width-flip' and not_already_augmented(img_path=img_path, augmentation=augmentation):
-                    img.transpose(PIL.Image.FLIP_LEFT_RIGHT).save(
-                        get_augmentation_path(
-                            img_path=img_path,
-                            augmentation=augmentation)
-                    )
-                elif augmentation == 'height-flip' and not_already_augmented(img_path=img_path,
-                                                                             augmentation=augmentation):
-                    img.transpose(PIL.Image.FLIP_TOP_BOTTOM).save(
-                        get_augmentation_path(
-                            img_path=img_path,
-                            augmentation=augmentation)
-                    )
-                elif augmentation == 'cwRotate' and not_already_augmented(img_path=img_path, augmentation=augmentation):
-                    img.transpose(PIL.Image.ROTATE_270).save(
-                        get_augmentation_path(
-                            img_path=img_path,
-                            augmentation=augmentation)
-                    )
-                elif augmentation == 'ccwRotate' and not_already_augmented(img_path=img_path,
-                                                                           augmentation=augmentation):
-                    img.transpose(PIL.Image.ROTATE_90).save(
-                        get_augmentation_path(
-                            img_path=img_path,
-                            augmentation=augmentation)
-                    )
-                elif augmentation == 'inverse' and not_already_augmented(img_path=img_path, augmentation=augmentation):
-                    img.transpose(PIL.Image.ROTATE_180).save(
-                        get_augmentation_path(
-                            img_path=img_path,
-                            augmentation=augmentation)
-                    )
+    @staticmethod
+    def augment_image(les_img_path, sub_augmented_path):
+        """ Apply augmentation operations defined by LES_AUGMENTATION corresponding to PIL transformations"""
+        global LES_AUGMENTATION
+        print(', '.join(LES_AUGMENTATION))
+        for i, img_path in enumerate(les_img_path):
+            with PIL.Image.open(img_path) as img:
+                print(str(i + 1) + "/" + str(len(les_img_path)) + " : Augmenting " + img_path.split("/")[-1] + " " * 30)
+                for augmentation in LES_AUGMENTATION:
+                    if augmentation == 'width-flip' and Augmentation.not_already_augmented(img_path=img_path,
+                                                                                           augmentation=augmentation,
+                                                                                           sub_augmented_path=sub_augmented_path):
+                        img.transpose(PIL.Image.FLIP_LEFT_RIGHT).save(
+                            Augmentation.get_augmentation_path(
+                                img_path=img_path,
+                                augmentation=augmentation,
+                                sub_augmented_path=sub_augmented_path)
+                        )
+                    elif augmentation == 'height-flip' and Augmentation.not_already_augmented(img_path=img_path,
+                                                                                              augmentation=augmentation,
+                                                                                              sub_augmented_path=sub_augmented_path):
+                        img.transpose(PIL.Image.FLIP_TOP_BOTTOM).save(
+                            Augmentation.get_augmentation_path(
+                                img_path=img_path,
+                                augmentation=augmentation,
+                                sub_augmented_path=sub_augmented_path)
+                        )
+                    elif augmentation == 'cwRotate' and Augmentation.not_already_augmented(img_path=img_path,
+                                                                                           augmentation=augmentation,
+                                                                                           sub_augmented_path=sub_augmented_path):
+                        img.transpose(PIL.Image.ROTATE_270).save(
+                            Augmentation.get_augmentation_path(
+                                img_path=img_path,
+                                augmentation=augmentation,
+                                sub_augmented_path=sub_augmented_path)
+                        )
+                    elif augmentation == 'ccwRotate' and Augmentation.not_already_augmented(img_path=img_path,
+                                                                                            augmentation=augmentation,
+                                                                                            sub_augmented_path=sub_augmented_path):
+                        img.transpose(PIL.Image.ROTATE_90).save(
+                            Augmentation.get_augmentation_path(
+                                img_path=img_path,
+                                augmentation=augmentation,
+                                sub_augmented_path=sub_augmented_path)
+                        )
+                    elif augmentation == 'inverse' and Augmentation.not_already_augmented(img_path=img_path,
+                                                                                          augmentation=augmentation,
+                                                                                          sub_augmented_path=sub_augmented_path):
+                        img.transpose(PIL.Image.ROTATE_180).save(
+                            Augmentation.get_augmentation_path(
+                                img_path=img_path,
+                                augmentation=augmentation,
+                                sub_augmented_path=sub_augmented_path)
+                        )
 
+    @staticmethod
+    def create_augmented_dataset():
+        """
+        Command augmented dataset creation and subfolders management
+        as train, validation and test to create their augmented dataset
+        :return:
+        """
+        create_folder(AUGMENTED_FOLDER)
+        les_img_path = []
 
-def main():
-    les_img_path = glob.glob(DATASET_FOLDER + '/**/*.jpg', recursive=True)
-    print("Augmenting images from " + DATASET_FOLDER + "\n\tto " + AUGMENTED_FOLDER)
-    label_folders = glob.glob(DATASET_FOLDER + "/*/")
-    for label_folder in label_folders:
-        label = label_folder.split("/")[-2]
-        create_folder(AUGMENTED_FOLDER + "/" + label)
-    augment_image(les_img_path)
-    print(" "*50 + "\nDone!")
+        if len(glob.glob(DATASET_PATH + "/*/*.jpg")) != 0:
+            print("Found label image folders in " + DATASET_PATH)
+            print("Augmenting without looking for potential subfolders as train, validation or test")
+            label_folders = glob.glob(DATASET_PATH + "/*")
+            for label_folder in label_folders:
+                print("Augmenting " + label_folder)
+                les_img_path = glob.glob(label_folder + "/*.jp*", recursive=True)
+                label = label_folder.split("/")[-1]
+                create_folder(AUGMENTED_FOLDER + "/" + label)
+                Augmentation.augment_image(les_img_path, DATASET_PATH)
+
+        else:
+            print("No label folders found in " + DATASET_PATH)
+            print("Looking at subfolders")
+            for sub_dataset_path in glob.glob(DATASET_PATH + "/*"):
+                sub_folder_name = sub_dataset_path.split("/")[-1]
+                sub_augmented_path = AUGMENTED_FOLDER + "/" + sub_folder_name
+                create_folder(sub_augmented_path)
+
+                # getting images in subfolder
+                label_folders = glob.glob(sub_dataset_path + "/*")
+                for label_folder in label_folders:
+                    print("Augmenting " + label_folder)
+                    les_img_path = glob.glob(label_folder + "/*.jp*", recursive=True)
+                    label = label_folder.split("/")[-1]
+                    create_folder(sub_augmented_path + "/" + label)
+                    Augmentation.augment_image(les_img_path, sub_augmented_path)
+                print("Done!\n\n")
 
 
 if __name__ == "__main__":
-    main()
+    Augmentation.create_augmented_dataset()
--- image_recognition/preprocess_dataset.py
+++ image_recognition/preprocess_dataset.py
@@ -4,8 +4,10 @@ import glob
 import os
 from image_recognition.preprocessing import Preprocessing
 from image_recognition.inception_classification.utilities import Errors
+import sys
 
 FOLDER_PATH = os.path.dirname(os.path.abspath(__file__))
+DATASET_PATH = FOLDER_PATH + "/dataset"
 
 
 class InitProject():
@@ -13,31 +15,35 @@ class InitProject():
         print("Initialization of the dataset")
         self.create_preprocessed_dataset()
 
-    def check_create_folder(self, path):
+    @staticmethod
+    def check_create_folder(path):
         if not os.path.exists(path):
             print("Creating folder " + path)
             os.mkdir(path)
 
-    def create_preprocessed_dataset(
-            self,
-            dataset_path=FOLDER_PATH + "/dataset/training",
-            preprocessed_dataset_path=FOLDER_PATH + "/preprocessed_dataset"
-    ):
-        print("Starting creation of the preprocessed dataset")
-        self.check_create_folder(preprocessed_dataset_path)
-        labels = [folder.split("/")[-2] for folder in glob.glob(dataset_path + "/*/")]
+    def subfolder_preprocessing(self, sub_dataset_path):
+        sub_preprocessed_dataset_path = sub_dataset_path.replace("/dataset/", "/preprocessed_dataset/")
+        print(sub_preprocessed_dataset_path)
+        self.check_create_folder(sub_preprocessed_dataset_path)
+        print("\n")
+        print("Preprocessing " + str(sub_dataset_path.split("/")[-2:-1]) + " to " + sub_preprocessed_dataset_path)
+
+        #  Getting labels
+        labels = [folder.split("/")[-2] for folder in glob.glob(sub_dataset_path + "/*/")]
         print("Found labels: " + str(labels))
 
         for label in labels:
             print("=== " + label + " ===")
-            self.check_create_folder("/".join([preprocessed_dataset_path, label]))
+            # Creating label dataset folder
+            self.check_create_folder("/".join([sub_preprocessed_dataset_path, label]))
+
             # getting image names from the folder
-            image_names = [file.split("/")[-1] for file in glob.glob(dataset_path + "/" + label + "/*.jp*")]
+            image_names = [file.split("/")[-1] for file in glob.glob(sub_dataset_path + "/" + label + "/*.jp*")]
             for image_name in image_names:
-                preprocessed_image_name = "crop_" + image_name
                 # generate path for original and preprocessed image
-                path_image = "/".join([dataset_path, label, image_name])
-                path_preprocessed_image = "/".join([preprocessed_dataset_path, label, preprocessed_image_name])
+                preprocessed_image_name = "crop_" + image_name
+                path_image = "/".join([sub_dataset_path, label, image_name])
+                path_preprocessed_image = "/".join([sub_preprocessed_dataset_path, label, preprocessed_image_name])
 
                 # Check if preprocessed was not already done
                 if not os.path.exists(path_preprocessed_image):
@@ -50,9 +56,34 @@ class InitProject():
                         print("\tCan't crop the image")
                     except KeyError:
                         print("- not vector found for " + path_preprocessed_image)
-                        # TODO create a formal error
                 else:
                     print("o Already preprocessed " + image_name)
+
+    def create_preprocessed_dataset(self):
+        """
+        Command preprocessed dataset creation and subfolders management
+        as train, validation and test to create their preprocessed dataset
+        :return:
+        """
+        #  Creating destination folder of preprocessed dataset
+        print("Starting creation of the preprocessed dataset")
+        preprocessed_dataset_path = FOLDER_PATH + "/preprocessed_dataset"
+        self.check_create_folder(preprocessed_dataset_path)
+
+        # Manage if we have plain dataset dataset/[label]/*.jpg] or subfolders dataset as dataset/train/[label]/*.jpg
+
+        if len(glob.glob(DATASET_PATH + "/*/*.jpg")) != 0:
+            print("Found label image folders in " + DATASET_PATH)
+            print("Augmenting without looking for potential subfolders as train, validation or test")
+            self.subfolder_preprocessing(sub_dataset_path=DATASET_PATH)
+        else:
+            print("No label folders found in " + DATASET_PATH)
+            print("Looking at subfolders")
+            #   Running preprocessed to train, validation and test folders:
+            for sub_dataset_path in glob.glob(DATASET_PATH + "/*"):
+                #  Create path name to preprocessed sub folder
+                print(sub_dataset_path)
+                self.subfolder_preprocessing(sub_dataset_path=sub_dataset_path)
 
 
 InitProject()
